fab60a7c7730b8f26c54d1cc599a7444d7def87c
SSt6708 <stevenliu6708@gmail.com>
2019-05-25 13:22:59 -0700

    making web request

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,7 +26,42 @@ class CreateAccountVC: UIViewController {
     }
     
 
-   
+    @IBAction func createAccountPressed(_ sender: Any) {
+        
+        guard let email = emailTxt.text, emailTxt.text != "" else {return}
+        
+        guard let pass = passTxt.text, passTxt.text != "" else {return}
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            
+            if success {
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success{
+                        print("logged in user!", AuthService.instance.authToke)
+                    }
+                })
+                
+            }
+            
+        }
+    }
+    
+    
+    
+    
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,32 @@
 
 import Foundation
 
+
+
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
 //Segue
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+//User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+//web url
+
+let BASE_URL = "https://smackkchatt.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+
+
+//headers
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
